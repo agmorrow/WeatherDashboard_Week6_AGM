7798bfc751fa8ec0b09ecb3c742d065d6320736a
agmorrow <morrow.austin5@gmail.com>
2021-12-14 10:58:53 -0700

    API working

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,11 +1,10 @@
-
 const searchBtn = document.querySelector('#searchBtn');
 const searchInput = document.querySelector('#searchInput');
 
 
 
 document.getElementById('searchBtn').addEventListener('click', function() {
-   fetch('https://api.openweathermap.org/data/2.5/weather?q='+searchInput.value+'')
+   fetch('https://api.openweathermap.org/data/2.5/weather?q='+searchInput.value+'&appid=8cdc26e92900b592593fbaf2a991cf6e')
    .then(response => response.json())
    .then(data => console.log(data))
    
