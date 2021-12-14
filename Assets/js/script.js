e0a9abcfa36155d712f0c9cb37c3bdb61aaabe3f
agmorrow <morrow.austin5@gmail.com>
2021-12-14 11:42:33 -0700

    DOM working

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,15 +1,30 @@
 const searchBtn = document.querySelector('#searchBtn');
 const searchInput = document.querySelector('#searchInput');
+const name = document.querySelector('.name');
+const temp = document.querySelector('.temp');
+const hum = document.querySelector('.hum');
+const wind = document.querySelector('.wind');
+const uv = document.querySelector('.uv');
+
+
 
 
 
 document.getElementById('searchBtn').addEventListener('click', function() {
    fetch('https://api.openweathermap.org/data/2.5/weather?q='+searchInput.value+'&appid=8cdc26e92900b592593fbaf2a991cf6e')
    .then(response => response.json())
-   .then(data => console.log(data))
-   
-   .catch(err => alert('Wrong city name!'))
+   .then(data => {
+      let nameValue = data['name'];
+      let tempValue = data['main']['temp'];
+      let windValue = data['wind']['speed'];
+      let humValue = data['main']['humidity'];
+      
+
+      name.innerHTML = nameValue;
+      temp.innerHTML = 'Temp: ' + tempValue;
+      wind.innerHTML = 'Wind: ' + windValue + ' MPH';
+      hum.innerHTML = 'Humidity: ' + humValue + ' %';
+
    })
    
-
-
+})
